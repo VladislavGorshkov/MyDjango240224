dcb9439bc2a80fe499fa369f81114598b3e28ab4
VladislavGorshkov <vlad110977@yandex.ru>
2024-02-24 15:30:53 +0300

    add home

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    text = """<h1>"Изучаем django"</h1> 
+           <strong>Автор</strong>: <i>Горшков В.В.</i>"""
+    return HttpResponse(text)
+
